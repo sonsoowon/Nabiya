8a58940b1ef11e77f11ef0ef10f81c4548fefd70
sonsoowon <yuwoo1018@gmail.com>
2022-05-25 12:41:56 +0900

    모델 생성

--- Nabiya/app/models.py
+++ Nabiya/app/models.py
@@ -32,6 +32,9 @@ class Pet(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="pets")
     name = models.CharField(max_length=50)
 
+    class Meta:
+        unique_together = (('owner', 'name'))
+
     def __str__(self):
         return self.name
 
