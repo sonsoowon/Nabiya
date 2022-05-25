e832ce69da91198ddc6be14194478f9ba7b632cb
sonsoowon <yuwoo1018@gmail.com>
2022-05-25 12:44:09 +0900

    함수 틀 생성

--- Nabiya/app/views.py
+++ Nabiya/app/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
+
+@login_required(login_url='register/login')
+def home(request):
+    pass
+
+def new_post(request):
+    pass
+
+def detail_post(request, post_pk):
+    pass
+
+@login_required(login_url='register/login')
+def mypage(request):
+    pass
+
+def list_following(request):
+    pass
+
+def list_follower(reqeust):
+    pass
+
+@login_required(login_url='register/login')
+def follow(request, user_pk):
+    pass
+
+
+def my_post(request):
+    pass
+
+# 검색어 입력 전에 게시물 나열
+# 검색어 입력 시 실시간 필터링 적용(JS)
+def search_user(request):
+    pass
+
+def login(request):
+    pass
+
+def signup(request):
+    pass
+
+def logout(request):
+    pass
