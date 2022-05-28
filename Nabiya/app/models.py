ce1f7a389bb0e99cf72239aad7a2131858a9b335
sonsoowon <yuwoo1018@gmail.com>
2022-05-28 13:01:03 +0900

    모델 주석 추가

--- Nabiya/app/models.py
+++ Nabiya/app/models.py
@@ -7,20 +7,20 @@ from django.utils.timezone import now
 # Create your models here.
 
 class Emotion(models.Model):
+
     EMOTIONS = (
         ('H', 'happy'),
         ('S', 'sad'),
         ('A', 'angry')
     )
     
-
     status = models.CharField(max_length=1, choices=EMOTIONS)
     emoji = models.ImageField(upload_to="emoji/")
 
     def __str__(self):
         return self.status
 
-
+# 동물 프로필 이미지 저장 경로 설정
 def profile_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/post/user_<id>/<filename>
     return 'pet_profile/user_{0}/{1}'.format(instance.user.id, filename)
@@ -43,7 +43,7 @@ class Pet(models.Model):
     def __str__(self):
         return self.name
 
-
+# 자주 기록하는 할 일 태그로 등록
 class Tag(models.Model):
     name = models.CharField(max_length=30)
     color = models.CharField(max_length=6)
@@ -53,10 +53,11 @@ class Tag(models.Model):
         return self.name
 
 
-
+# 일기 사진 저장 경로 설정
 def photo_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/post/user_<id>/<filename>
     return 'diary/user_{0}/{1}'.format(instance.user.id, filename)
+
 
 class Diary(models.Model):
     writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
@@ -66,14 +67,14 @@ class Diary(models.Model):
     photo = models.ImageField(upload_to=photo_directory_path)
     content = models.TextField(max_length=100)
 
-
+# Post 모델은 추후에 구현(우선 건드리지 맙시다)
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
     photo = models.ImageField(upload_to="post/%Y/%m/%d/")
     uploaded = models.DateField(auto_now_add=True)
 
-
+# Post 모델 구현할 때 같이 구현
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     writer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
