6be2d6504bfed958f51c28253a8c048daf6c4b64
sonsoowon <yuwoo1018@gmail.com>
2022-05-28 12:04:42 +0900

    수정: 기본 user 모델 사용

--- Nabiya/Nabiya/settings.py
+++ Nabiya/Nabiya/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-AUTH_USER_MODEL = 'app.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
